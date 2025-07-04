260b2ae61be2df608b5348fb7c5c97dc02ec87ed
unknown <shunjr2012@gmail.com>
2025-07-03 21:53:39 -0400

    Initial Commit

---
+++ Source/SurvivalGame1/SurvivalGame1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SurvivalGame1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivalGame1, "SurvivalGame1" );
